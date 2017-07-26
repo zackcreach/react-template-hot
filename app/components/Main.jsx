1b8a5517a1754cafb2a6974528625905a9d2af52
zackcreach <zackcreach@gmail.com>
2017-07-26 11:00:26 -0400

    Submit button up and running!

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,23 +4,40 @@ import 'styles/styles';
 class Main extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      videoURL: 'https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761',
+      emailAddress: ''
+    }
   }
-  emailSubmit(e) {
+  componentWillMount() {
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      emailAddress: e.target.value
+    });
+  }
+  handleSubmit(e) {
     e.preventDefault();
+    console.log(this.state.emailAddress);
   }
   render() {
     return(
       <div className='Main'>
-        <div className='Main__video'>
-          <video preload='auto' loop='' src='https://player.vimeo.com/external/9514605.hd.mp4?s=fb9d73a58ff23539e5a836f22d8361e1ed936ccf'></video>
+        <div className='Main__video-container'>
+          <video className='Main__video' muted autoPlay loop>
+            <source src={this.state.videoURL} type='video/mp4'></source>
+          </video>
         </div>
         <div className='Main__mask'></div>
         <div>
           <div className='Main__content'>
             <h1 className='Main__title'>All Our Former Selves</h1>
-            <form className='Main__email-form'>
-              <input className='Main__email' placeholder='Email Address'></input>
-              <button className='Main__button' onSubmit={this.emailSubmit}>Submit</button>
+            <form className='Main__email-form' onSubmit={this.handleSubmit}>
+              <input className='Main__email' ref='active' value={this.state.emailAddress} onChange={this.handleChange} placeholder='Email Address'></input>
+              <input className='Main__button' type='submit' value='Submit'/>
             </form>
           </div>
         </div>
