2870315b56d99770f226eeb55ca1a9409ba8f588
zackcreach <zackcreach@gmail.com>
2017-07-26 09:24:06 -0400

    Progress on landing page.

--- README.md
+++
@@ -1,3 +0,0 @@
-# react-template-hot
-
-A well-commented react template featuring HMR/react-hot-loader, css-hot-loader, css modules, and lots more using webpack 3.
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,18 +1,29 @@
 import React, {Component} from 'react';
 import 'styles/styles';
-import test from 'images/test';
 
 class Main extends Component {
   constructor(props) {
     super(props);
   }
+  emailSubmit(e) {
+    e.preventDefault();
+  }
   render() {
     return(
-      <div>
-        {/* Use this method for multiple classes with CSS modules (not currently enabled in webpack) */}
-        {/* <h1 className={`${S.Main} ${S.Main__bold}`}>Main Component</h1> */}
-        <h1 className='Main Main__bold'>Main Component</h1>
-        <img className='Main__image' src={test}/>
+      <div className='Main'>
+        <div className='Main__video'>
+          <video preload='auto' loop='' src='https://player.vimeo.com/external/9514605.hd.mp4?s=fb9d73a58ff23539e5a836f22d8361e1ed936ccf'></video>
+        </div>
+        <div className='Main__mask'></div>
+        <div>
+          <div className='Main__content'>
+            <h1 className='Main__title'>All Our Former Selves</h1>
+            <form className='Main__email-form'>
+              <input className='Main__email' placeholder='Email Address'></input>
+              <button className='Main__button' onSubmit={this.emailSubmit}>Submit</button>
+            </form>
+          </div>
+        </div>
       </div>
     )
   }
