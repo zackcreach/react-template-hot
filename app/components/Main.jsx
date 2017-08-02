62ce7c43cb3140427bab6e3deeab6481360443c4
zackcreach <zackcreach@gmail.com>
2017-08-02 10:30:06 -0400

    Reverting changes back to template mode.

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,46 +1,18 @@
 import React, {Component} from 'react';
 import 'styles/styles';
+import test from 'images/test';
 
 class Main extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      videoURL: 'https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761',
-      emailAddress: ''
-    }
-  }
-  componentWillMount() {
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleChange(e) {
-    this.setState({
-      emailAddress: e.target.value
-    });
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(this.state.emailAddress);
   }
   render() {
     return(
-      <div className='Main'>
-        <div className='Main__video-container'>
-          <video className='Main__video' muted autoPlay loop>
-            <source src={this.state.videoURL} type='video/mp4'></source>
-          </video>
-        </div>
-        <div className='Main__mask'></div>
-        <div>
-          <div className='Main__content'>
-            <h1 className='Main__title'>All Our Former Selves</h1>
-            <form className='Main__email-form' onSubmit={this.handleSubmit}>
-              <input className='Main__email' ref='active' value={this.state.emailAddress} onChange={this.handleChange} placeholder='Email Address'></input>
-              <input className='Main__button' type='submit' value='Submit'/>
-            </form>
-          </div>
-        </div>
+      <div>
+        {/* Use this method for multiple classes with CSS modules (not currently enabled in webpack) */}
+        {/* <h1 className={`${S.Main} ${S.Main__bold}`}>Main Component</h1> */}
+        <h1 className='Main Main__bold'>Main Component</h1>
+        <img className='Main__image' src={test}/>
       </div>
     )
   }
